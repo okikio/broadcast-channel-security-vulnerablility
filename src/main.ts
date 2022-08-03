5404c59b1ed5a76df80d54ede5420d0ae01641d2
okikio <okikio.dev@gmail.com>
2022-08-03 00:25:44 -0400

    2 files modified, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
-import WebWorker from './worker.ts?worker'; 
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import WebWorker from './worker.ts?worker';
 
-new WebWorker();
+const app = document.querySelector<HTMLDivElement>('#app');
 
-const broadchannel = new BroadcastChannel("broad-cast");
-broadchannel.onmessage = ({ data }) => { 
-  console.log(data)
-} 
-broadchannel.postMessage("From Main Thread");
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+app!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -21,12 +14,24 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+new WebWorker();
+
+const broadchannel = new BroadcastChannel('broad-cast');
+broadchannel.onmessage = ({ data }) => {
+  console.log(data);
+
+  const div = document.createElement('div');
+  div.innerText = data;
+  app!.appendChild(div);
+};
+
+setTimeout(() => {
+  broadchannel.postMessage('From Main Thread');
+}, 100);
--- src/worker.ts
+++ src/worker.ts
@@ -1,16 +1,21 @@
-'use strict';
-
-const code = (msg: string) => `
-const broadchannel = new BroadcastChannel('broad-cast');
+export const code = (msg: string) => `
+let broadchannel = new BroadcastChannel('broad-cast');
 broadchannel.onmessage = ({ data }) => {
   console.log(data);
 };
 broadchannel.postMessage('From ${msg} in Worker');
 `;
 
-eval(code('Eval'));
-(0, eval)(code('In-Direct Eval'));
-Function(`${code('Function')} return broadchannel`);
-Function(`"use strict";\n${code('Use-Strict Function')} return broadchannel`);
+function evaluate() {
+  'use strict';
 
-export {};
+  eval(code('Eval'));
+  Function(`${code('Function')} return broadchannel`)();
+
+  (0, eval)(code('In-Direct Eval'));
+  Function(
+    `"use strict";\n${code('Use-Strict Function')} return broadchannel`
+  )();
+}
+
+evaluate();
